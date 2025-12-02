08692f505feacff84853c4544eb3e802d8feac83
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2025-12-02 18:18:56 -0500

    Commented coordinateFormat.js

--- src/utils/coordinateFormat.js
+++ src/utils/coordinateFormat.js
@@ -1,3 +1,5 @@
+// This formats all location coordinates into a [latitude, longitude] array. Example: [44.3429282° N, 79.6911183° W]. 
+// During processing, the location format may change into alternate formats. This code standardizes the format in the database.
 export function coordinateFormat(locationObject) {
     if (!locationObject) return null;
 
