7a7847e1aaffba66388a8c1eec3be44fe29a352c
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2025-12-02 18:23:53 -0500

    Commented getDistanceInKm.js

--- src/utils/getDistanceInKm.js
+++ src/utils/getDistanceInKm.js
@@ -1,4 +1,5 @@
-// DISTANCE CALCULATION (User to restaurant)
+// Calculates the distance between two location coordinate arrays. 
+// It is used to calculate whether a user is within ordering range of a restaurant and for estimate time of arrival (ETA) calculations.
 export function getDistanceInKm(lat1, lon1, lat2, lon2) {
   const R = 6371; // Radius of the Earth in km
   const dLat = ((lat2 - lat1) * Math.PI) / 180;
@@ -10,5 +11,5 @@ export function getDistanceInKm(lat1, lon1, lat2, lon2) {
       Math.sin(dLon / 2) ** 2;
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return parseFloat((R * c).toFixed(2));; // distance in km, rounded to 2 decimals
+  return parseFloat((R * c).toFixed(2));; //
 }
